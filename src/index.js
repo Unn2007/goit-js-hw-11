7a07da3cfabe90bb2f78163fe9d9725d618aa925
Unn2007 <unn2007@i.ua>
2023-04-25 17:33:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,67 +1,24 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const axios = require('axios/dist/browser/axios.cjs');
-console.log(axios);
-axios.get('/user?ID=12345')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
+
+const searchParams = new URLSearchParams({
+  key:"35759118-aa5f76bc84c3135be08c1161d",
+  q:"fish",
+  image_type:"photo",
+  orientation: "horizontal",
+  safesearch: true,
+  per_page:40,
+  page:2
   });
 
 
-// function fetchUsers() {
-//     return axios.get('https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name')
-//   .then(function (response) {
-//     if (!response.ok) {
-//               throw new Error(response.status);
-//             }
-//             return response.json();
+  async function getUser() {
+          const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
+          return await response.data;
    
-//   })
+  }
+  getUser().then((data)=>console.log(data));
+
+
   
-// };
-// fetchUsers().then((users) => console.log(users))
-//     .catch((error) => console.log(error));
-
-// const fetchUsersBtn = document.querySelector(".btn");
-// const userList = document.querySelector(".user-list");
-
-// fetchUsersBtn.addEventListener("click", () => {
-//   fetchUsers()
-//     .then((users) => renderUserList(users))
-//     .catch((error) => console.log(error));
-// });
-
-// function fetchUsers() {
-//   return fetch(
-//     "https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name"
-//   ).then((response) => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
-
-// function renderUserList(users) {
-//   const markup = users
-//     .map((user) => {
-//       return `
-//           <li>
-//             <p><b>Name</b>: ${user.name}</p>
-//             <p><b>Email</b>: ${user.email}</p>
-//             <p><b>Company</b>: ${user.company.name}</p>
-//           </li>
-//       `;
-//     })
-//     .join("");
-//   userList.innerHTML = markup;
-// }
-
-// Resources
